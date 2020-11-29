6e3ab0eea341545a46459f2c7bed866028682b1a
thebicx <christianprince132@gmail.com>
2020-11-29 21:12:46 +1100

    Vector buffer method

--- maths/vector/vec.h
+++ maths/vector/vec.h
@@ -6,4 +6,10 @@ namespace math
 template<unsigned int length, typename T>
 class vec;
 
+template<unsigned int length, typename T>
+const T* buffer(const vec<length, T>& vector)
+{
+    return &(vector.x);
+}
+
 }
